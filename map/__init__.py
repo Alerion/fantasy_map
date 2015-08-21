e6f7d4d09dcad06b2cae45a5a1bd86c749d55a82
Dmitriy Kostochko <alerion.um@gmail.com>
2015-08-21 16:37:57 +0300

    add setup.py

---
+++ map/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1'
+VERSION = __version__
+
+__description__ = 'Map generator'
---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+
+    use_setuptools()
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(
+    name='fantasy-map',
+    version=__import__('map').__version__,
+    description=__import__('map').__description__,
+    long_description=README,
+    license='The MIT License (MIT)',
+    author='Dmitriy Kostochko',
+    author_email='alerionum+django@gmail.com',
+    packages=find_packages(exclude=['fantasy_map', 'fantasy_map.*']),
+    url='https://github.com/Alerion/fantasy_map',
+    keywords='Django, GIS',
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ]
+)
